e4e136636a1ffb34076e6691c281c973846200df
Roman Fedusevych <romanfyks@gmail.com>
2021-10-31 00:01:26 +0300

    add more genres

--- src/components/MovieList/MovieList.tsx
+++ src/components/MovieList/MovieList.tsx
@@ -9,6 +9,11 @@ const options = [
   { value: '35', label: 'Comedy' },
   { value: '80', label: 'Crime' },
   { value: '18', label: 'Drama' },
+  { value: '14', label: 'Fantasy' },
+  { value: '36', label: 'History' },
+  { value: '27', label: 'Horror' },
+  { value: '53', label: 'Thriller' },
+  { value: '37', label: 'Western' },
 ];
 
 type Props = {
